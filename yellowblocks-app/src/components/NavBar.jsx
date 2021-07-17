9671ffc4431018d72fb35faa5ef235a8b8afdfbd
Austin Vuong <austin.vuong00@gmail.com>
2021-07-17 23:13:45 +1000

    Avatar

--- yellowblocks-app/src/components/NavBar.jsx
+++ yellowblocks-app/src/components/NavBar.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Layout, Menu, Breadcrumb } from 'antd';
 
-const { Header, Content, Footer } = Layout;
-
+const { Header } = Layout;
 function NavBar() {
   return (
     <>
       <Header>
-        <div className="logo" />
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-        <Menu.Item>LearnAbility</Menu.Item>
+        <Menu 
+        theme="dark" 
+        mode="horizontal"
+        inlineIndent={0}
+        style={{display: 'flex', justifyContent: 'space-between', width: '100%', }}
+        >
+            <Menu.Item>LearnAbility</Menu.Item>
+            <Menu.Item>
+              <img src="./avatar.png" style={{width: '50px', height: '50px', borderRadius: '50%'}}/>
+            </Menu.Item>
         </Menu>
       </Header>
     </>
