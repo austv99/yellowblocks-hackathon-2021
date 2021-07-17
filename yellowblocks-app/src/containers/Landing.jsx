03450d230cee5d04dadbb9afa2b44bfefa45cb88
Austin Vuong <austin.vuong00@gmail.com>
2021-07-17 17:32:33 +1000

    nav

---
+++ yellowblocks-app/src/containers/Landing.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+import NavBar from '../components/NavBar'
+
+function Landing () {
+    return (
+        <>
+        <NavBar />
+        </>
+    );
+
+}
+
+export default Landing;
